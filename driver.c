ddbcf8a431e1f45bfe074691c838e19e462021de
ssSally111 <2392341633@qq.com>
2023-12-11 22:47:42 +0800

    update

--- driver.c
+++ driver.c
@@ -1,19 +1,64 @@
 #include <ntddk.h>
 
-void DriverUnload(PDRIVER_OBJECT pDriverObject);
+
+#define DEVICE_NAME L"\\Device\\TestReadDriver"
+#define SYM_LINK_NAME L"\\??\\TestRead"
+
+
+NTSTATUS DriverUnload(PDRIVER_OBJECT pDriverObject);
+NTSTATUS DriverRead(PDEVICE_OBJECT pDriverObject, PIRP pIrp);
+
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 {
 	DbgPrint("[DriverTest] DriverEntry\n");
-	NTSTATUS status = STATUS_SUCCESS;
 	pDriverObject->DriverUnload = DriverUnload;
+
+	NTSTATUS status = STATUS_SUCCESS;
+
+	PDEVICE_OBJECT pDevice;
+	UNICODE_STRING DeviceName;
+	RtlInitUnicodeString(&DeviceName, DEVICE_NAME);
+	status = IoCreateDevice(pDriverObject, 0, &DeviceName, FILE_DEVICE_UNKNOWN, 0, TRUE, &pDevice);
+	if (!NT_SUCCESS(status))
+	{
+		DbgPrint("[DriverTest] CreateDevice Failed\n");
+		return status;
+	}
+
+	UNICODE_STRING SymLinkName = RTL_CONSTANT_STRING(SYM_LINK_NAME);
+	status = IoCreateSymbolicLink(&SymLinkName, &DeviceName);
+	if (!NT_SUCCESS(status))
+	{
+		DbgPrint("[DriverTest] CreateSymbolicLink Failed\n");
+		IoDeleteDevice(pDevice);
+		return status;
+	}
+
+	pDevice->Flags |= DO_BUFFERED_IO;
+	pDriverObject->MajorFunction[IRP_MJ_READ] = DriverRead;
 
 	return status;
 }
 
-void DriverUnload(PDRIVER_OBJECT pDriverObject)
+NTSTATUS DriverUnload(PDRIVER_OBJECT pDriverObject)
 {
 	DbgPrint("[DriverTest] DriverUnload\n");
 
-	return;
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS DriverRead(PDEVICE_OBJECT pDriverObject, PIRP pIrp)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+
+	PIO_STACK_LOCATION pIoStack = IoGetCurrentIrpStackLocation(pIrp);
+	ULONG ulReadLength = pIoStack->Parameters.Read.Length;
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = ulReadLength;
+	memset(pIrp->AssociatedIrp.SystemBuffer, 0xAA, ulReadLength);
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+	DbgPrint("[DriverTest] DriverRead OK");
+	return status;
 }
