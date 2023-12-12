8278501ba9871ad1d67d3eb317affbf77b2eb3d7
ssSally111 <2392341633@qq.com>
2023-12-12 22:50:48 +0800

    update: add api KillProcess

--- driver.c
+++ driver.c
@@ -3,26 +3,32 @@
 
 #define DEVICE_NAME L"\\Device\\DriverTest"
 #define SYM_LINK_NAME L"\\??\\DriverControlsTest"
+#define IOCTL_TEST CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
 
 NTSTATUS DriverUnload(PDRIVER_OBJECT pDriverObject);
+NTSTATUS MajorHandle(PDEVICE_OBJECT pDriverObject, PIRP pIrp);
+NTSTATUS DriverControl(PDEVICE_OBJECT pDriverObject, PIRP pIrp);
 NTSTATUS DriverRead(PDEVICE_OBJECT pDriverObject, PIRP pIrp);
-NTSTATUS DriverCreate(PDEVICE_OBJECT pDriverObject, PIRP pIrp);
-
-PDEVICE_OBJECT pDevice;
-UNICODE_STRING SymLinkName;
+NTSTATUS DriverWrite(PDEVICE_OBJECT pDriverObject, PIRP pIrp);
+NTSTATUS KillProcess(ULONG pid);
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 {
 	DbgPrint("[DriverTest] DriverEntry\n");
 
+	for (size_t i = 0; i < IRP_MJ_MAXIMUM_FUNCTION; ++i)
+	{
+		pDriverObject->MajorFunction[i] = MajorHandle;
+	}
+	pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DriverControl;
 	pDriverObject->DriverUnload = DriverUnload;
-	pDriverObject->MajorFunction[IRP_MJ_READ] = DriverRead;
-	pDriverObject->MajorFunction[IRP_MJ_CREATE] = DriverCreate;
 
 	NTSTATUS status = STATUS_SUCCESS;
-
+	PDEVICE_OBJECT pDevice;
+	UNICODE_STRING SymLinkName;
 	UNICODE_STRING DeviceName;
+
 	RtlInitUnicodeString(&DeviceName, DEVICE_NAME);
 	status = IoCreateDevice(pDriverObject, 0, &DeviceName, FILE_DEVICE_UNKNOWN, 0, TRUE, &pDevice);
 	if (!NT_SUCCESS(status))
@@ -47,22 +53,61 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegPath)
 
 NTSTATUS DriverUnload(PDRIVER_OBJECT pDriverObject)
 {
+	UNICODE_STRING SymLinkName;
+	RtlInitUnicodeString(&SymLinkName, SYM_LINK_NAME);
+
 	IoDeleteSymbolicLink(&SymLinkName);
-	IoDeleteDevice(pDevice);
+	IoDeleteDevice(pDriverObject->DeviceObject);
 
 	DbgPrint("[DriverTest] DriverUnload\n");
 	return STATUS_SUCCESS;
 }
 
-NTSTATUS DriverCreate(PDEVICE_OBJECT pDriverObject, PIRP pIrp)
+NTSTATUS MajorHandle(PDEVICE_OBJECT pDriverObject, PIRP pIrp)
 {
 	NTSTATUS status = STATUS_SUCCESS;
 
-	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Status = status;
 	pIrp->IoStatus.Information = 0;
 	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
 
-	DbgPrint("[DriverTest] DriverCreate OK");
+	return status;
+}
+
+NTSTATUS DriverControl(PDEVICE_OBJECT pDriverObject, PIRP pIrp)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+
+	PIO_STACK_LOCATION pIrps = IoGetCurrentIrpStackLocation(pIrp);
+	ULONG inLength = pIrps->Parameters.DeviceIoControl.InputBufferLength;
+	ULONG optLength = pIrps->Parameters.DeviceIoControl.OutputBufferLength;
+	ULONG CODE = pIrps->Parameters.DeviceIoControl.IoControlCode;
+	ULONG info = 0;
+
+	switch (CODE)
+	{
+	case IOCTL_TEST:
+	{
+		DbgPrint("[DriverTest] DriverControl IOCTL_TEST");
+
+		PVOID pBuff = pIrp->AssociatedIrp.SystemBuffer;
+
+		ULONG pid = *(PLONG)(pBuff);
+		NTSTATUS status = KillProcess(pid);
+		memset(pBuff, status, 8);
+		info = 8;
+
+		break;
+	}
+	default:
+		break;
+	}
+
+	pIrp->IoStatus.Status = status;
+	pIrp->IoStatus.Information = info;
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+	DbgPrint("[DriverTest] DriverControl SUCCESS");
 	return status;
 }
 
@@ -72,11 +117,54 @@ NTSTATUS DriverRead(PDEVICE_OBJECT pDriverObject, PIRP pIrp)
 
 	PIO_STACK_LOCATION pIoStack = IoGetCurrentIrpStackLocation(pIrp);
 	ULONG ulReadLength = pIoStack->Parameters.Read.Length;
+	PVOID lpAddr = MmGetSystemAddressForMdlSafe(pIrp->MdlAddress, NormalPagePriority);
+
 	pIrp->IoStatus.Status = STATUS_SUCCESS;
 	pIrp->IoStatus.Information = ulReadLength;
-	memset(pIrp->AssociatedIrp.SystemBuffer, 0x0C, ulReadLength);
 	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
 
-	DbgPrint("[DriverTest] DriverRead OK");
+	if (!lpAddr)
+	{
+		return status;
+	}
+
+	//memset(pIrp->AssociatedIrp.SystemBuffer, 0x0C, ulReadLength);
+	memset(lpAddr, 0x61, ulReadLength - 2);
+	((PWCHAR)lpAddr)[ulReadLength - 1] = L'\0';
+
+	DbgPrint("[DriverTest] DriverRead SUCCESS");
+	return status;
+}
+
+NTSTATUS DriverWrite(PDEVICE_OBJECT pDriverObject, PIRP pIrp)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	pIrp->IoStatus.Information = 0;
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+	DbgPrint("[DriverTest] DriverWrite SUCCESS");
+	return status;
+}
+
+NTSTATUS KillProcess(ULONG pid)
+{
+	NTSTATUS status = STATUS_SUCCESS;
+	HANDLE ProcessHandle;
+	OBJECT_ATTRIBUTES ObjAttr;
+	CLIENT_ID cid;
+	cid.UniqueProcess = (HANDLE)pid;
+	cid.UniqueThread = 0;
+
+	InitializeObjectAttributes(&ObjAttr, 0, 0, 0, 0);
+	status = ZwOpenProcess(&ProcessHandle, PROCESS_ALL_ACCESS, &ObjAttr, &cid);
+	if (NT_SUCCESS(status))
+	{
+		ZwTerminateProcess(ProcessHandle, status);
+		ZwClose(ProcessHandle);
+	}
+
+	DbgPrint("[DriverTest] KillProcess PID: %d", pid);
 	return status;
 }
